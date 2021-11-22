b40f05fcaca6260a1630ff794a8f01a14397f3d1
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-22 21:20:30 +0600

    Add files via upload

---
+++ area.py
@@ -0,0 +1,13 @@
+#Calculation of triangle area;
+
+base = float(input(("Enter your triangle base :")))
+hight = float(input(("Enter your triangle hight :")))
+area =(0.5*hight*base)
+print("Triangle area = ",area)
+
+
+#Calculation of circle area;
+
+radius = float(input(("Enter your circle radius :")))
+area1 = 3.14*radius*radius
+print("Circle area = ",area1)
